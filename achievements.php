f68108220784fee1a1e0e31d576c1844d78f14ba
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2020-08-18 10:11:31 +0530

    added new acievement description

--- achievements.php
+++ achievements.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html class="no-js" lang="en-US">
+
 <head>
 
   <!-- DEFAULT META TAGS -->
@@ -10,29 +11,31 @@
   <!-- CSS -->
   <link rel="stylesheet" type="text/css" href="files/css/custom-music.css">
   <link rel="stylesheet" type="text/css" href="files/css/bootstrap_grid.css">
-  <link rel="stylesheet" id="default-style-css"  href="files/css/style.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="default-style-css" href="files/css/style.css" type="text/css" media="all" />
   <link rel="stylesheet" id="fontawesome-style-css" href="files/css/font-awesome.min.css" type="text/css" media="all" />
   <link rel="stylesheet" id="ionic-icons-style-css" href="files/css/ionicons.css" type="text/css" media="all" />
-  <link rel="stylesheet" id="isotope-style-css"  href="files/css/isotope.css" type="text/css" media="all" />
-  <link rel="stylesheet" id="mqueries-style-css"  href="files/css/mqueries.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="isotope-style-css" href="files/css/isotope.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="mqueries-style-css" href="files/css/mqueries.css" type="text/css" media="all" />
 
-  <link rel="stylesheet" id="common_inner"  href="files/css/common_inner.css" type="text/css" media="all" />
+  <link rel="stylesheet" id="common_inner" href="files/css/common_inner.css" type="text/css" media="all" />
 
   <style type="text/css">
-    body{
+    body {
       background-image: url('img/bg-main.jpg');
       background-size: cover;
       background-attachment: fixed;
     }
 
-    .custom-col-achi *{
+    .custom-col-achi * {
       vertical-align: middle;
     }
-    .custom-col-achi{
+
+    .custom-col-achi {
       margin-top: 60px;
       margin-bottom: 60px;
     }
-    .custom-col-achi .ion{
+
+    .custom-col-achi .ion {
       font-size: 1.5rem;
       transition: all 0.5s ease;
       -webkit-transition: all 0.5s ease;
@@ -40,46 +43,55 @@
       -o-transition: all 0.5s ease;
       -ms-transition: all 0.5s ease;
     }
-    .custom-col-achi .achi-box:hover .ion{
+
+    .custom-col-achi .achi-box:hover .ion {
       color: #da2d19;
     }
-    .custom-col-achi .colored strong{
+
+    .custom-col-achi .colored strong {
       color: #fff;
     }
-    .achi-box{
-      border-bottom: 1px solid rgba(0,0,0,0.12);
+
+    .achi-box {
+      border-bottom: 1px solid rgba(0, 0, 0, 0.12);
       padding: 30px 10px;
       cursor: default;
-      background:rgba(191, 191, 191, 0.2);
+      background: rgba(191, 191, 191, 0.2);
       border-radius: 10px;
       margin-bottom: 10px;
     }
-    .achi-trophy{
+
+    .achi-trophy {
       display: inline-block;
       margin-right: 10px;
     }
-    .achi-event-name{
+
+    .achi-event-name {
       display: inline-block;
     }
-    .achi-desc{
+
+    .achi-desc {
       display: block;
       margin-left: 40px;
       color: #f2f2f2;
     }
-    .hoverable:hover{
+
+    .hoverable:hover {
       transition: box-shadow .25s;
-      box-shadow: 0px 5px 6px 0 rgba(0, 0, 0, 0.12), 0px 0px 0px 0 rgba(0,0,0,0.19);
+      box-shadow: 0px 5px 6px 0 rgba(0, 0, 0, 0.12), 0px 0px 0px 0 rgba(0, 0, 0, 0.19);
     }
-    .achi-date{
-    }
-    .button-div{
+
+    .achi-date {}
+
+    .button-div {
       -webkit-transition: all 0.5s ease;
       -ms-transition: all 0.5s ease;
       -o-transition: all 0.5s ease;
       transition: all 0.5s ease;
     }
-    @media(min-width: 769px){
-      .custom-col-achi{
+
+    @media(min-width: 769px) {
+      .custom-col-achi {
         width: 60%;
         margin-left: auto;
         margin-right: auto;
@@ -94,13 +106,16 @@
 <body>
 
   <div id="fb-root"></div>
-  <script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=297540167315276";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
+  <script>
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=297540167315276";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  </script>
   <div class="container">
     <div class="row">
       <br>
@@ -125,19 +140,37 @@
             <i class="ion ion-trophy"></i>
           </div>
           <div class="achi-event-name">
+            <span class="colored achi-event-name"><strong>BAJA SAE LOUISVILLE, <span class="achi-date">2020</span></strong></span>
+          </div>
+          <div class="achi-desc">
+            <ul>
+              <li>4th Overall Internationally</li>
+              <li>6th in Design Presentation</li>
+              <li>8th in Cost Event</li>
+              <li>12th in Sales Presentation</li>
+              <li>Team Jaabaz has become one of the few International teams to present a FOUR WHEEL DRIVE ATV for the first time ever.</li>
+              <li>We are the 1st amongst all Indian teams to achieve such a rank for a FOUR WHEEL DRIVE ATV.</li>
+            </ul>
+          </div>
+        </div>
+        <div class="achi-box hoverable">
+          <div class="achi-trophy">
+            <i class="ion ion-trophy"></i>
+          </div>
+          <div class="achi-event-name">
             <span class="colored achi-event-name"><strong>BAJA SAE CALIFORNIA, <span class="achi-date">2019</span></strong></span>
           </div>
           <div class="achi-desc">
-          <ul>
-						<li>4th in Sales Presentation</li>
-						<li>12th in Cost Event</li>
-						<li>15th in Design Presentation</li>
-						<li>9th Overall Static</li>
-						<li>28th in Maneuverability</li>
-					</ul>
+            <ul>
+              <li>4th in Sales Presentation</li>
+              <li>12th in Cost Event</li>
+              <li>15th in Design Presentation</li>
+              <li>9th Overall Static</li>
+              <li>28th in Maneuverability</li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div class="achi-box hoverable">
+        <div class="achi-box hoverable">
           <div class="achi-trophy">
             <i class="ion ion-trophy"></i>
           </div>
@@ -145,10 +178,10 @@
             <span class="colored achi-event-name"><strong>BAJA SAE KANSAS, <span class="achi-date">2018</span></strong></span>
           </div>
           <div class="achi-desc">
-          <ul>
-							<li>18th in Design Presentation</li>
-							<li>24th in Sales Presentation</li>
-						</ul>
+            <ul>
+              <li>18th in Design Presentation</li>
+              <li>24th in Sales Presentation</li>
+            </ul>
           </div>
         </div>
         <div class="achi-box hoverable">
@@ -159,13 +192,13 @@
             <span class="colored achi-event-name"><strong>ENDURO STUDENT INDIA, <span class="achi-date">2018</span></strong></span>
           </div>
           <div class="achi-desc">
-          <ul>
-							<li>7th in Design Presentation</li>
-							<li>7th in Maneuverability</li>
-							<li>5th in DixtX</li>
-							<li>5th in Overall Dynamics</li>
-							<li>7th Overall Nationally</li>
-						</ul>
+            <ul>
+              <li>7th in Design Presentation</li>
+              <li>7th in Maneuverability</li>
+              <li>5th in DixtX</li>
+              <li>5th in Overall Dynamics</li>
+              <li>7th Overall Nationally</li>
+            </ul>
           </div>
         </div>
         <div class="achi-box hoverable">
@@ -176,12 +209,12 @@
             <span class="colored achi-event-name"><strong>BAJA SAE CALIFORNIA, <span class="achi-date">2017</span></strong></span>
           </div>
           <div class="achi-desc">
-          <ul>
-							<li>20th in Design Event</li>
-							<li>14th in Cost Event</li>
-							<li>14th in Sales Presentation</li>
-							<li>27th Overall Internationally</li>
-						</ul>
+            <ul>
+              <li>20th in Design Event</li>
+              <li>14th in Cost Event</li>
+              <li>14th in Sales Presentation</li>
+              <li>27th Overall Internationally</li>
+            </ul>
           </div>
         </div>
         <div class="achi-box hoverable">
@@ -257,7 +290,7 @@
       </div>
     </div>
   </div>
-              <!--   <td><i class="ion ion-trophy"></i></td>
+  <!--   <td><i class="ion ion-trophy"></i></td>
                 <td><span class="colored achi-event-name"><strong>OESA GENERATION AUTO VIDEO CONTEST</strong></span></td>
                 <td>2nd Place</td>
              
@@ -271,40 +304,42 @@
           <!--********END MAIN CONTENT*********!-->
 
 
-          <!-- FOOTER -->  
-          <?php include('includes/inner_footer.php');
-          ?>
-          <!-- FOOTER -->
+  <!-- FOOTER -->
+  <?php include('includes/inner_footer.php');
+  ?>
+  <!-- FOOTER -->
 
-        </div> <!-- END #page-content -->
-        <!-- PAGE CONTENT -->
+  </div> <!-- END #page-content -->
+  <!-- PAGE CONTENT -->
 
-        <!-- SCRIPTS -->
-        <script src="files/js/jquery-2.1.4.min.js"></script>
-        <script type="text/javascript" src="files/js/jquery.easing.1.3.js"></script>
-        <script type="text/javascript" src="files/js/jquery.visible.min.js"></script>
-        <script type="text/javascript" src="files/js/script.js"></script>
-        <script type="text/javascript">
+  <!-- SCRIPTS -->
+  <script src="files/js/jquery-2.1.4.min.js"></script>
+  <script type="text/javascript" src="files/js/jquery.easing.1.3.js"></script>
+  <script type="text/javascript" src="files/js/jquery.visible.min.js"></script>
+  <script type="text/javascript" src="files/js/script.js"></script>
+  <script type="text/javascript">
     //<![CDATA[
-        $(window).on('load', function() { // makes sure the whole site is loaded 
-            $('#status').fadeOut(); // will first fade out the loading animation 
-            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-            $('body').delay(350).css({'overflow':'visible'});
-          })
+    $(window).on('load', function() { // makes sure the whole site is loaded 
+      $('#status').fadeOut(); // will first fade out the loading animation 
+      $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+      $('body').delay(350).css({
+        'overflow': 'visible'
+      });
+    })
     //]]>
   </script>
   <script type="text/javascript">
-    var gobackTop = $('.button-div').offset().top;  
-    $(window).scroll(function() {                  // assign scroll event listener
+    var gobackTop = $('.button-div').offset().top;
+    $(window).scroll(function() { // assign scroll event listener
 
-    var currentScroll = $(window).scrollTop(); // get current position
-    if (currentScroll >= gobackTop) {           // apply position: fixed if you
-        $('.button-div').css({                      // scroll to that element or below it
+      var currentScroll = $(window).scrollTop(); // get current position
+      if (currentScroll >= gobackTop) { // apply position: fixed if you
+        $('.button-div').css({ // scroll to that element or below it
           position: 'fixed',
           top: '5px'
         });
-    } else {                                   // apply position: static
-        $('.button-div').css({                      // if you scroll above it
+      } else { // apply position: static
+        $('.button-div').css({ // if you scroll above it
           position: 'static'
         });
       }
@@ -313,4 +348,5 @@
   <!-- SCRIPTS -->
 
 </body>
+
 </html>
